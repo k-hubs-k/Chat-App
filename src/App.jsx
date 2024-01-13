08c914470106aa6026d048c037ab7f09e6baab62
Hubs <raherihubert@gmail.com>
2024-01-13 22:01:40 +0300

    Adding home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,74 @@ import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const Root = () => {
+  return (
+    <div className="container">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "chatApp",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "chat",
+        element: <Home />,
+      },
+      {
+        path: "search",
+        element: <Home />,
+      },
+      {
+        path: "settings",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Home />,
+      },
+      {
+        path: "logout",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        path: "authentification",
+        element: <Outlet />,
+        children: [
+          {
+            path: "signIn",
+            element: <SignIn />,
+          },
+          {
+            path: "signUp",
+            element: <SignUp />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <>
-      <NavBar />
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,39 +5,43 @@ import {
   AccountCircleOutlined,
   QuestionAnswer,
   SearchRounded,
-  ArrowLeft,
+  ArrowForwardIosOutlined,
+  ArrowBackIosNewOutlined,
 } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const NavBar = () => {
   const links = [
-    { name: "Chat App", to: "#", component: <QuestionAnswer /> },
+    { name: "Chat App", to: "/", component: <QuestionAnswer /> },
     { name: "Chat", to: "chat", component: <ChatBubbleOutlineOutlined /> },
     { name: "Search", to: "search", component: <SearchRounded /> },
     { name: "Settings", to: "settings", component: <SettingsOutlined /> },
     { name: "Profile", to: "profile", component: <AccountCircleOutlined /> },
     { name: "Logout", to: "Logout", component: <LogoutOutlined /> },
   ];
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="container">
-      <div className="navigation">
-        <ul>
-          {links.map((item, key) => {
-            return (
-              <li key={key}>
-                <a href="#">
-                  <span className="icon">{item.component}</span>
-                  <span className="title">{item.name}</span>
-                </a>
-              </li>
-            );
-          })}
-          <li className="toggle">
-            <span className="icon">
-              <ArrowLeft />
-            </span>
-          </li>
-        </ul>
-      </div>
+    <div className={`navigation ${isOpen ? "open" : ""} `}>
+      <ul>
+        {links.map((item, key) => {
+          return (
+            <li key={key}>
+              <NavLink to={item.to}>
+                <span className="icon">{item.component}</span>
+                <span className="title">{item.name}</span>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+      <button className="toggle" onClick={toggle}>
+        {!isOpen ? <ArrowForwardIosOutlined /> : <ArrowBackIosNewOutlined />}
+      </button>
     </div>
   );
 };
