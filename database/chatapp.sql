101b02f200319cbde44743a0cde5280aa9e69561
fadhel <fadhelubain@gmail.com>
2024-01-23 10:42:01 +0000

    fusion

--- database/chatapp.sql
+++ database/chatapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : dim. 14 jan. 2024 à 19:25
+-- Généré le : mar. 23 jan. 2024 à 09:36
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -33,15 +33,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL,
+  `images` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'fadhel', 'fadhel@gmail.com', 'fdl123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `images`) VALUES
+(1, 'fadhel', 'fadhel@gmail.com', 'fdl123', 'image_1706001754964.jpg'),
+(2, 'gg', 'fdd@gmail.com', '12345', NULL),
+(3, 'hubs', 'hubs@gmail.com', 'hu12', NULL),
+(4, 'nas', 'nas@gmail.com', '123.gg', NULL),
+(5, 'ff', 'ff@gmail.com', 'ff123', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
